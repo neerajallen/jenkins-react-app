f0b9bd64def418baf214e7be6caa05182b0d9491
neerajallen <100480612+neerajallen@users.noreply.github.com>
2022-12-06 12:29:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h2>Allendigital  </h2>
+      <h2>Allen Career Institute Private Limited  </h2>
     </div>
   );
 }
