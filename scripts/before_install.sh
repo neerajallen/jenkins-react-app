2ae17efd62eb456d5a96efccc8dcf11c0608cb0d
neerajallen <100480612+neerajallen@users.noreply.github.com>
2023-03-17 17:04:18 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#_Change_Working_Directory
+cd /home/ubuntu/server
+
+#_Update_&_Set_Node_Version
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+
+#_Download_Node_&NPM
+apt-get -y install nodejs npm
+
+#_Download_PM2
+npm install pm2@latest -g
