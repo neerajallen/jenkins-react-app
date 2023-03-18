26ace2a8c302b587ca775a393f4b55f6c86b97a6
neerajallen <100480612+neerajallen@users.noreply.github.com>
2023-03-18 12:41:10 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,8 +4,8 @@
 cd /home/ubuntu/server
 
 #_Update_&_Set_Node_Version
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-#curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+#curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 
 #_Download_Node_&NPM
 apt-get -y install nodejs npm
